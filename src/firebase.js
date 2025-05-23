8aa1017c76ff50be65bb1fe268e94410a408c066
sujitha.sakthivel@timelapseinformatics.com <sujitha.sakthivel@timelapseinformatics.com>
2025-05-23 15:40:56 +0530

    env created

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDABKc2k9U0ezhvPoyQq8POLQDpMmoO7E",
-  authDomain: "esthell-properties.firebaseapp.com",
-  projectId: "esthell-properties",
-  storageBucket: "esthell-properties.firebasestorage.app",
-  messagingSenderId: "4113990017",
-  appId: "1:4113990017:web:c9b88b5b8149492d3a2880",
-  measurementId: "G-ZEP191FXLD"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
