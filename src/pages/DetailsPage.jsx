aadbd7f23794c0513a77afe9881b1f921f42df16
karthik-kerry <karthikeyan.ns@timelapseinformatics.com>
2025-06-13 17:03:50 +0530

    minor fix

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -355,7 +355,6 @@ export default function DetailsPage() {
     },
   ];
 
-  //update
   const progressWidth = (rating / 5) * 100;
   const images = [Property, Property1];
   const [currentIndex, setCurrentIndex] = useState(0);
